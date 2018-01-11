989070ca3be1a2383bbaacddaaf3741796d005cc
subidsha <31504698+subidsha@users.noreply.github.com>
2018-01-11 10:12:56 +0000

    Create past.c

---
+++ past.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number\n",n);
+scanf("%d",n);
+if(n>1)
+{
+printf("the number is positive",n);
+}
+else 
+{
+printf("the number is negative",n);
+}
+}
